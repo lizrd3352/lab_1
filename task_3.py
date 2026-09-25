d7df0892e35aca588c6ebc9e8a4a921afee5a951
lizrd3352 <l1zochka.sh@yandex.com>
2026-09-25 20:59:30 +0300

    Create task_3.py

---
+++ task_3.py
@@ -0,0 +1,47 @@
+x = 17
+y = 5
+
+print('Эксперимент А')
+print(x + y, type(x + y))
+print(x - y, type(x - y))
+print(x * y, type(x * y))
+print(x / y, type(x / y))
+print(x // y, type(x // y))
+print(x % y, type(x % y))
+print(x ** y, type(x ** y))
+
+print()
+
+print('Эксперимент В')
+print(2 ** 1000)
+
+print()
+
+print('Эксперимент С')
+import math
+result = 0.1 + 0.2
+print(result)
+print(result == 0.3)
+print(result - 0.3)
+print(math.isclose(result, 0.3))
+
+print()
+
+print('Эскперимент D')
+i = int('42')
+f = float('3.14')
+s = str(2026)
+b0 = bool(0)
+b1 = bool(-1)
+b = bool('')
+bf = bool('False')
+c = complex(2, -3)
+
+print(i, type(i))
+print(f, type(f))
+print(s, type(s))
+print(b0, type(b0))
+print(b1, type(b1))
+print(b, type(b))
+print(bf, type(bf))
+print(c, type(c))
