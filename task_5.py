450d030f8513b8fbba18561d961396a665056699
lizrd3352 <l1zochka.sh@yandex.com>
2026-09-26 09:01:52 +0300

    Update task_5.py

--- task_5.py
+++ task_5.py
@@ -1,11 +1,21 @@
 name = input('Введите ваше имя: ')
 exp = input('Введите название эксперимента: ')
-zap = input('Введите количество запусков: ')
-vr = input('Введите время одного запуска: ')
+zap = int(input('Введите количество запусков: '))
+vr = float(input('Введите время одного запуска: '))
 cc = input('Введите части комплексного числа: ').split()
 has_runs = bool(zap)
 
+overall_time_sec = vr * zap
+overall_time_min = vr * zap / 60
+coefficient = complex(float(cc[0]), float(cc[1]))
+kv_module = (float(cc[0]) ** 2) + (float(cc[1]) ** 2)
+
 print('========================================', f'ЭКСПЕРИМЕНТ: {exp}', f'Исследователь: {name}',
-      f'Запуски: {zap}', f'Общее время: {float(vr) * float(zap)} ({float(vr) * (int(zap)) / 60} мин)',
-      f'Коэффицент: {complex(float(cc[0]), float(cc[1]))}', f'Квадрат модуля: {(float(cc[0]) ** 2) + (float(cc[1]) ** 2)}',
+      f'Запуски: {zap}', f'Общее время: {overall_time_sec:.2f} c ({overall_time_min:.2f} мин)',
+      f'Коэффицент: {coefficient}', f'Квадрат модуля: {kv_module:.2f}',
       f'Есть выполненные запуски: {has_runs}', '========================================', sep='\n')
+
+print('Диагностика!', f'Исследователь: {name} ==> {type(name)}', f'Эксперимент: {exp} == > {type(exp)}',
+      f'Запуски: {zap} ==> {type(zap)}', f'Коэффицент: {coefficient} ==> {type(coefficient)}',
+      f'Выполненные запуски: {has_runs} ==> {type(has_runs)}', f'Квадрат модуля: {kv_module} ==> {type(kv_module)}',
+      '========================================', sep='\n')
